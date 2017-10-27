bcb82b62f7a8cc25d7642b52d4c05c7d522aae04
bruno nobre <bnobre@rastreabilidadebrasil.com.br>
2017-10-27 16:47:21 -0200

    get post del

---
+++ src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa Historia</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Nossa Missao</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+    </div>
+)
